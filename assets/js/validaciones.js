d685b6e2bf4cc04c9f9bb06e5587b0cf92f15808
Tomas Catalano <74085420+TomasC753@users.noreply.github.com>
2023-10-18 22:57:59 -0300

    API2: Field validation use swith

--- assets/js/validaciones.js
+++ assets/js/validaciones.js
@@ -1,45 +1,47 @@
 class Field {
-    constructor({fieldName, value, rules = []}) {
+    constructor({ fieldName, value, rules = [] }) {
         this.fieldName = fieldName;
         this.value = value;
         this.rules = rules;
     }
 
     validate() {
-        return this.rules.map(rule => {
-            if (rule == 'required') {
-                if (this.value == null || this.value == '') {
-                    return `El campo \"${this.fieldName}\" es requerido`;
-                }
-                return;
+        const errorMessages = this.rules.map(rule => {
+            switch (rule) {
+                case 'required':
+                    if (this.value === null || this.value === '') {
+                        return `El campo "${this.fieldName}" es requerido`;
+                    }
+                    break;
+                case 'numeric':
+                    if (isNaN(this.value)) {
+                        return `El campo "${this.fieldName}" debe ser de tipo numérico`;
+                    }
+                    break;
+                case 'id':
+                    if (this.value == '0') {
+                        return `El campo "${this.fieldName}" es requerido`;
+                    }
+                    break
+                default:
+                    if (/^<(-?\d+)$/.test(rule)) {
+                        const numero = parseInt(rule.slice(1));
+                        if (this.value >= numero) {
+                            return `El valor del campo "${this.fieldName}" debe ser menor que ${numero}`;
+                        }
+                    } else if (/^>(-?\d+)$/.test(rule)) {
+                        const numero = parseInt(rule.slice(1));
+                        if (this.value <= numero) {
+                            return `El valor del campo "${this.fieldName}" debe ser mayor que ${numero}`;
+                        }
+                    } else if (rule === 'id' && this.value === 0) {
+                        return `El campo "${this.fieldName}" es requerido`;
+                    }
             }
-            if (rule == 'numeric') {
-                if (isNaN(this.value)) {
-                    return `El campo \"${this.fieldName}\" debe ser del tipo numérico`;
-                }
-                return;
-            }
-            if (/\<((\-)?[0-9]*)/.test(rule)) {
-                const numero = parseInt(rule.slice(1));
-                if (this.value >= numero) {
-                    return `El valor del campo \"${this.fieldName}\" debe ser menor a ${numero}`;
-                }
-                return;
-            }
-            if (/\>((\-)?[0-9]*)/.test(rule)) {
-                const numero = parseInt(rule.slice(1));
-                if (this.value <= numero) {
-                    return `El valor del campo \"${this.fieldName}\" debe ser mayor a ${numero}`;
-                }
-                return;
-            }
-            if (rule == 'id') {
-                if (this.value == 0) {
-                    return `El campo \"${this.fieldName}\" es requerido`;
-                }
-                return;
-            }
-        }).filter(Boolean);
+            return null;
+        }).filter(message => message !== null);
+
+        return errorMessages;
     }
 }
 
@@ -47,30 +49,46 @@ class Validation {
     constructor(...fields) {
         this.fields = fields;
     }
-    
+
     validate() {
-        const fieldErrors = this.fields.map(field => field.validate()).flat();
+        const fieldErrors = this.fields.map((field) => field.validate()).flat();
         const result = fieldErrors.length === 0;
-        const message = fieldErrors.join('\n');
+        const message = fieldErrors.join("\n");
         return { result, message };
     }
 }
 
-$('document').ready(function () {
+$("document").ready(function () {
     //
-    $('#productSave').click((event) => {
+    $("#productSave").click((event) => {
         event.preventDefault();
-        let productName = $('#productName').val();
-        let productPrice = $('#productPrice').val();
-        let productDescription = $('#productDescription').val();
-        let productCategory = $('#productCategory').val();
-        let productImage = $('#productImage').val();
+        let productName = $("#productName").val();
+        let productPrice = $("#productPrice").val();
+        let productDescription = $("#productDescription").val();
+        let productCategory = $("#productCategory").val();
+        let productImage = $("#productImage").val();
 
         let { result, message } = new Validation(
-            new Field({fieldName: 'Nombre del producto', value: productName, rules: ['required']}),
-            new Field({fieldName: 'Precio del producto', value: productPrice, rules: ['required', 'numeric', '>-1']}),
-            new Field({fieldName: 'Categoría del producto', value: productCategory, rules: ['required', 'id']}),
-            new Field({fieldName: 'Imagen del producto', value: productImage, rules: ['required']})
+            new Field({
+                fieldName: "Nombre del producto",
+                value: productName,
+                rules: ["required"],
+            }),
+            new Field({
+                fieldName: "Precio del producto",
+                value: productPrice,
+                rules: ["required", "numeric", ">-1"],
+            }),
+            new Field({
+                fieldName: "Categoría del producto",
+                value: productCategory,
+                rules: ["required", "id"],
+            }),
+            new Field({
+                fieldName: "Imagen del producto",
+                value: productImage,
+                rules: ["required"],
+            })
         ).validate();
 
         if (!result) {
@@ -78,15 +96,19 @@ $('document').ready(function () {
             return;
         }
 
-        alert('Correcto');
+        alert("Correcto");
     });
 
-    $('#categorySave').click((event) => {
+    $("#categorySave").click((event) => {
         event.preventDefault();
-        let categoryName = $('#categoryName').val();
+        let categoryName = $("#categoryName").val();
 
         let { result, message } = new Validation(
-            new Field({fieldName: 'Nombre de la categoría', value: categoryName, rules: ['required']}),
+            new Field({
+                fieldName: "Nombre de la categoría",
+                value: categoryName,
+                rules: ["required"],
+            })
         ).validate();
 
         if (!result) {
@@ -94,6 +116,6 @@ $('document').ready(function () {
             return;
         }
 
-        alert('Correcto');
+        alert("Correcto");
     });
 });
